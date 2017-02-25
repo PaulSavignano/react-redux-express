434f9bbd4233ffcee4e26cdd3e4ff57b95b96dba
Paul Savignano <Paul.Savignano@gmail.com>
2017-02-25 08:02:56 -0800

    Add todos reducer and tests

--- client/src/reducers/todoReducers.js
+++ client/src/reducers/todoReducers.js
@@ -1,3 +1,6 @@
+import uuidV1 from 'uuid/v1'
+import moment from 'moment'
+
 export const todoSearchReducer = (state = '', action) => {
   switch (action.type) {
     case 'SET_TODO_SEARCH':
@@ -11,6 +14,35 @@ export const showCompletedReducer = (state = false, action) => {
   switch (action.type) {
     case 'TOGGLE_SHOW_COMPLETED':
       return !state
+    default:
+      return state
+  }
+}
+
+export const todosReducer = (state = [], action) => {
+  switch (action.type) {
+    case 'TODO_ADD':
+      return [
+        ...state,
+        {
+          uuid: uuidV1(),
+          text: action.text,
+          completed: false,
+          createdAt: moment().unix(),
+          completedAt: undefined
+        }
+      ]
+    case 'TOGGLE_TODO':
+      return state.map((todo) => {
+        if (todo.uuid === action.uuid) {
+          const nextCompleted = !todo.completed
+          return {
+            ...todo,
+            completed: nextCompleted,
+            completedAt: nextCompleted ? moment().unix() : undefined
+          }
+        }
+      })
     default:
       return state
   }
--- client/src/reducers/todoReducers.test.js
+++ client/src/reducers/todoReducers.test.js
@@ -1,6 +1,6 @@
 import expect from 'expect'
 import deepFreeze from 'deep-freeze-strict'
-import { todoSearchReducer, showCompletedReducer } from './todoReducers'
+import { todoSearchReducer, showCompletedReducer, todosReducer } from './todoReducers'
 
 describe('todoReducers', () => {
   describe('todoSearchReducer', () => {
@@ -20,6 +20,29 @@ describe('todoReducers', () => {
       }
       const res = showCompletedReducer(deepFreeze(false), deepFreeze(action))
       expect(res).toEqual(true)
+    })
+  })
+  describe('todosReducer', () => {
+    it('should add a new todo', () => {
+      const action = {
+        type: 'TODO_ADD',
+        text: 'Walk Pepper'
+      }
+      const res = todosReducer(deepFreeze([]), deepFreeze(action))
+      expect(res.length).toEqual(1)
+      expect(res[0].text).toEqual(action.text)
+    })
+    it('should toggle todo', () => {
+      const todos = [{
+        uuid: '123', text: 'Something', completed: true, createdAt: 123, completedAt: 125
+      }]
+      const action = {
+        type: 'TOGGLE_TODO',
+        uuid: '123'
+      }
+      const res = todosReducer(deepFreeze(todos), deepFreeze(action))
+      expect(res[0].completed).toEqual(false)
+      expect(res[0].completedAt).toEqual(undefined)
     })
   })
 })
